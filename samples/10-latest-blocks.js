31326668a4e029b0bfd0d6cc350892ac51238f0b
Matthieu Riou <matthieu@offthelip.org>
2014-03-30 22:11:10 -0700

    Added block example and transactions.

---
+++ samples/10-latest-blocks.js
@@ -0,0 +1,24 @@
+// Displays the block by rendering a div summarizing some of its most important details
+function showBlock(b) {
+  var total = b.total / 100000000
+  $('#10-latest-blocks').before("<div>Block at "+b.height+" transfered "+ total+" in "+b.n_tx+" transactions.</div>");
+}
+
+// Gets the JSON data returned by the previous JQuery.get, parse it and returns a
+// new promise to get the next block.
+function printAndGetNext(data) {
+  var block = JSON.parse(data);
+  showBlock(block);
+  return $.get(block.prev_block_url);
+}
+
+// Gets the blockchain data and parse it, returning a promise to get the latest block
+var initiate = $.get("http://api.blockcypher.com/v1/btc/main").then(function(data) {
+  var chain = JSON.parse(data);
+  return $.get(chain.latestUrl);
+});
+
+// We continue by looping on the promise returned above and chaining 10 calls to print
+// the block and get the next one.
+var next = initiate;
+for (var n = 0; n < 10; n++) { next = next.then(printAndGetNext); }
---
+++ samples/3-latest-blocks.js
@@ -0,0 +1,21 @@
+// Displays the block, for now we just do an alert to show the block's height.
+function showBlock(block) {
+  alert(block.height);
+}
+
+// Gets the JSON data returned by the previous JQuery.get, parse it and returns a
+// new promise to get the next block.
+function printAndGetNext(data) {
+  var block = JSON.parse(data);
+  showBlock(block);
+  return $.get(block.prev_block_url);
+}
+
+// First gets the blockchain data using JQuery.get, parses it and returns a promise to get
+// the latest block. We queue several then() calls right after to get each block one after
+// another and display them.
+$.get("http://api.blockcypher.com/v1/btc/main").then(function(data) {
+  var chain = JSON.parse(data);
+  return $.get(chain.latestUrl);
+}).then(printAndGetNext).then(printAndGetNext).then(printAndGetNext);
+
