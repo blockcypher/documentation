77fafd58f95a6dde05fc7bed5c38ec74ebe21c15
Matthieu Riou <matthieu@offthelip.org>
2014-03-31 22:26:05 -0700

    Wording finick.

--- samples/browser-websocket.js
+++ samples/browser-websocket.js
@@ -6,8 +6,8 @@ ws.onmessage = function (event) {
   var total = tx.total / 100000000;
   var addrs = tx.addresses.join(", ");
   console.log(tx.addresses)
-  $('#browser-websocket').before("<div>Unconfirmed transaction " + shortHash + " of " + total +
-                                   " involving addresses " + addrs + "</div>");
+  $('#browser-websocket').before("<div>Unconfirmed transaction " + shortHash + " totalling " + total +
+                                   "BTC involving addresses " + addrs + "</div>");
   count++;
   if (count > 10) ws.close();
 }
