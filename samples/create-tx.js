9b3a03f54498d77a65b2a276e11946c0bf0574cc
Matthieu Riou <matthieu@offthelip.org>
2014-04-21 16:23:54 -0700

    More details in tx signing section. First pass as a tx creation sample in JS using bitcoinjs.

---
+++ samples/create-tx.js
@@ -0,0 +1,48 @@
+var rootUrl = "https://api.blockcypher.com/v1/btc/test3";
+var source = {
+  private : "ef05a415ea4097a63d9cc285b1834272726a24c42090b83e2944f6490093e6ab1101a538e67d",
+  public  : "02c8d29d8c8afb4725d25a1ec496aeddc36fa9eb6f0e014bfc21742fc3d78cb438",
+  address : "mwmabpJVisvti3WEP5vhFRtn3yqHRD9KNP"
+}
+var dest = null;
+
+function parseAddr(jsonAddr) {
+  dest = JSON.parse(jsonAddr);
+}
+
+function newTransaction() {
+  var newtx = {
+    "inputs": [{"addresses": [source.address]}],
+    "outputs": [{"addresses": [dest.address], "value": 25000}]
+  }
+  return $.post(rootUrl+"/tx/new");
+}
+
+function signAndSend(newtxjson) {
+  var newtx   = JSON.parse(newtxjson);
+  if (newtx.errors && newtx.errors.length) {
+    console.log("Errors occured!!/n", newtx.errors.join("/n"));
+    return;
+  }
+
+  var key     = Bitcoin.ECKey.fromHex(source.private);
+  var pubkeys = [];
+  var signatures = newtx.tosign.map(function(tosign) {
+    pubkeys.push(source.public);
+    return key.sign(tosign).concat(1);
+  });
+
+  newtx.signatures  = signatures;
+  newtx.pubkeys     = pubkeys;
+  return $.post(rootUrl+"/addrs", JSON.stringify(newtx));
+}
+
+function print(finaltxjson) {
+  var finaltx = JSON.parse(newtxjson);
+  if (newtx.errors && newtx.errors.length) {
+    console.log("Errors occured!!/n", newtx.errors.join("/n"));
+    return;
+  }
+}
+
+$.post(rootUrl+"/addrs").then(parseAddr).then(newTransaction).then(signAndSend).then(print);
