37ebbcec203a712a819d03fb42045b31eb072448
Matthieu Riou <matthieu@offthelip.org>
2014-04-22 14:50:21 -0700

    Externalized CSS. Fixes and improvements to sending transaction sample.

--- samples/create-tx.js
+++ samples/create-tx.js
@@ -1,13 +1,14 @@
 var rootUrl = "https://api.blockcypher.com/v1/btc/test3";
 var source = {
-  private : "ef05a415ea4097a63d9cc285b1834272726a24c42090b83e2944f6490093e6ab1101a538e67d",
+  private : "05a415ea4097a63d9cc285b1834272726a24c42090b83e2944f6490093e6ab11",
   public  : "02c8d29d8c8afb4725d25a1ec496aeddc36fa9eb6f0e014bfc21742fc3d78cb438",
   address : "mwmabpJVisvti3WEP5vhFRtn3yqHRD9KNP"
 }
 var dest = null;
 
-function parseAddr(jsonAddr) {
-  dest = JSON.parse(jsonAddr);
+function logAddr(addr) {
+  dest = addr;
+  log("Generated new address " + dest.address)
 }
 
 function newTransaction() {
@@ -15,13 +16,12 @@ function newTransaction() {
     "inputs": [{"addresses": [source.address]}],
     "outputs": [{"addresses": [dest.address], "value": 25000}]
   }
-  return $.post(rootUrl+"/txs/new");
+  return $.post(rootUrl+"/txs/new", JSON.stringify(newtx));
 }
 
-function signAndSend(newtxjson) {
-  var newtx   = JSON.parse(newtxjson);
+function signAndSend(newtx) {
   if (newtx.errors && newtx.errors.length) {
-    console.log("Errors occured!!/n", newtx.errors.join("/n"));
+    log("Errors occured!!/n" + newtx.errors.join("/n"));
     return;
   }
 
@@ -29,7 +29,7 @@ function signAndSend(newtxjson) {
   var pubkeys = [];
   var signatures = newtx.tosign.map(function(tosign) {
     pubkeys.push(source.public);
-    return key.sign(tosign).concat(1);
+    return Bitcoin.convert.bytesToHex(key.sign(Bitcoin.convert.hexToBytes(tosign)).concat(1));
   });
 
   newtx.signatures  = signatures;
@@ -37,17 +37,21 @@ function signAndSend(newtxjson) {
   return $.post(rootUrl+"/txs/send", JSON.stringify(newtx));
 }
 
-function print(finaltxjson) {
-  var finaltx = JSON.parse(newtxjson);
-  if (newtx.errors && newtx.errors.length) {
-    console.log("Errors occured!!/n", newtx.errors.join("/n"));
+function print(finaltx) {
+  if (finaltx.errors && finaltx.errors.length) {
+    log("Errors occured!!/n" + newtx.errors.join("/n"));
     return;
   }
-  console.log("Transaction to", dest.address, "sending", finaltx.total/100000000 , "BTC sent.");
+  log("Transaction hash: " + finaltx.tx.hash)
+  log("Transaction to " + dest.address + " of " + finaltx.tx.outputs[0].value/100000000 + " BTC sent.");
+}
+
+function log(msg) {
+  $("div.log").append("<div>" + msg + "</div>")
 }
 
 $.post(rootUrl+"/addrs")
-  .then(parseAddr)
+  .then(logAddr)
   .then(newTransaction)
   .then(signAndSend)
   .then(print);
