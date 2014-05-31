3101d4c31d2850ef99a2b15b424dbb2c7ad2f588
Matthieu Riou <matthieu@offthelip.org>
2014-05-31 15:01:04 -0700

    Updated testnet address.

--- samples/create-tx.js
+++ samples/create-tx.js
@@ -5,9 +5,9 @@ var rootUrl = "https://api.blockcypher.com/v1/btc/test3";
 // please do not drain our test account, if you need testnet BTC use a faucet
 // https://tpfaucet.appspot.com/
 var source = {
-  private : "05a415ea4097a63d9cc285b1834272726a24c42090b83e2944f6490093e6ab11",
-  public  : "02c8d29d8c8afb4725d25a1ec496aeddc36fa9eb6f0e014bfc21742fc3d78cb438",
-  address : "mwmabpJVisvti3WEP5vhFRtn3yqHRD9KNP"
+  private : "1af97b1f428ac89b7d35323ea7a68aba8cad178a04eddbbf591f65671bae48a2",
+  public  : "03bb318b00de944086fad67ab78a832eb1bf26916053ecd3b14a3f48f9fbe0821f",
+  address : "mtWg6ccLiZWw2Et7E5UqmHsYgrAi5wqiov"
 }
 var key   = Bitcoin.ECKey.fromHex(source.private);
 var dest  = null;
